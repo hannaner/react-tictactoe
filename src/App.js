819c5f8e5f200019be9aaf4aac6ee0ff06759af7
Hanna Kim <hanna.kim.x@gmail.com>
2023-07-06 23:36:14 +0000

    Cleanup states to avoid redundancy, set const xIsNext

--- src/App.js
+++ src/App.js
@@ -2,27 +2,27 @@ import { useState } from "react";
 import Board from "./Board";
 
 export default function Game() {
-  // state for determining player
-  const [xIsNext, setXIsNext] = useState(true);
   // state for play history
   const [history, setHistory] = useState([Array(9).fill(null)]);
   // ???? why is this useState holding the Array explicitly in '[]' compared to the state in
   // const [squares, setSquares] = useState(Array(9).fill(null));
   const [currentMove, setCurrentMove] = useState(0);
+  // state for determining player
+  const xIsNext = currentMove % 2 === 0;
+  // render current move
   const currentSquares = history[currentMove];
 
   // function to determine if there is a winner or who's turn is next
+  // track new history when a previous move is visited
   function handlePlay(nextSquares) {
-    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
-    setHistory(nextHistory)
-    setCurrentMove(nextHistory.length - 1)
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
 
   // play history
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
